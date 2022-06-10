ed510abdfbae305a2eab8bae20d80b9f68a8baec
Joseph Broome <josephlloydbroome@gmail.com>
2022-06-10 11:49:08 -0700

    iteration

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        for (int i = 0; i<15; i++)
+        {
+            Console.WriteLine(i);
+        }
+
+
+
+
+    
+    }
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
